470bc8f31450ccfa224aa2af28e667c60f08f00e
Randkill <randkill.randkill@gmail.com>
2020-10-30 20:42:06 +0330

    Added add functionality + delete + using map to add layer

--- Visual-Keras-Model-Generator/cheatsheet/FrontEnd/frontend01/src/App.js
+++ Visual-Keras-Model-Generator/cheatsheet/FrontEnd/frontend01/src/App.js
@@ -1,20 +1,62 @@
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 import Header from "./Components/Header/Header.js";
 import Layer from "./Components/Layer/Layer.js";
 
-function App() {
-  return (
-    <div>
-      <Header />
+class App extends Component {
+  state = {
+    layers: []
+  }
 
+  addLayer = () => {
+    console.log('Button Clicked !')
+
+    const tempLayers = [...this.state.layers]
+    const layerId = Date.now()
+    tempLayers.push({info: '', id: layerId});
+    this.setState({layers: tempLayers})
+    //console.log('New Layers count:', tempLayers[0])
+    console.log(this.state)
+  }
+
+  deleteLayer = (index) => {
+    const tempLayers = [...this.state.layers]
+    tempLayers.splice(index, 1)
+
+    this.setState({layers: tempLayers})
+  }
+
+  render() {
+    let Layers = null
+    
+    Layers = (
       <div className="Layers">
-        <Layer />
-        <Layer />
+        {
+          this.state.layers.map((layer, index) => {
+            return(
+              <Layer layerNumber={index}
+              delete={() => this.deleteLayer(index)}
+              key={layer.id}
+              />
+            )
+          })
+        }
       </div>
-    </div>
-  );
+    )
+
+    return (
+      <div>
+        <Header />
+
+        <div className="Layers">
+          {Layers}
+          <button className="Button" onClick={this.addLayer}>+</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;

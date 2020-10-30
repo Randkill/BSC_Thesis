5203a08662f4c38dfc3bac4eebba2ad1e478c269
Randkill <randkill.randkill@gmail.com>
2020-10-30 20:41:26 +0330

    Added layer number + delete button

--- Visual-Keras-Model-Generator/cheatsheet/FrontEnd/frontend01/src/Components/Layer/Layer.js
+++ Visual-Keras-Model-Generator/cheatsheet/FrontEnd/frontend01/src/Components/Layer/Layer.js
@@ -3,14 +3,21 @@ import React from "react";
 import "./Layer.css";
 
 const Layer = (props) => {
-    return(
-        <div className="Layer">
-            <p>
-                name
-            </p>
-            <input/>
-        </div>
-    )
+  return (
+    <div className="Layer">
+      <p className="LayerTitle">Layer {props.layerNumber+1}</p>
+      <p>Number of Neurons</p>
+      <input />
+      <p>Activation Function</p>
+      <select id="activationFunction" name="activationFunction">
+        <option value="relu">relu</option>
+        <option value="sigmoid">sigmoid</option>
+        <option value="softmax">softmax</option>
+        <option value="softplus">softplus</option>
+      </select>
+      <button onClick={props.delete}>-</button>
+    </div>
+  );
 };
 
 export default Layer;

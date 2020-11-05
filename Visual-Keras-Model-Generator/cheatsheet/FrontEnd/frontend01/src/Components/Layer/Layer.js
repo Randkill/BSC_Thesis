eca29fb7fb82cdb2c9bf47285bddbff7e224dbc4
Randkill <randkill.randkill@gmail.com>
2020-11-05 23:21:43 +0330

    Added handler for SELECT tag!

--- Visual-Keras-Model-Generator/cheatsheet/FrontEnd/frontend01/src/Components/Layer/Layer.js
+++ Visual-Keras-Model-Generator/cheatsheet/FrontEnd/frontend01/src/Components/Layer/Layer.js
@@ -3,13 +3,18 @@ import React from "react";
 import "./Layer.css";
 
 const Layer = (props) => {
+
+  const selectHandler = (event) => {
+    return event.target.value;
+  }
+
   return (
     <div className="Layer">
       <p className="LayerTitle">Layer {props.layerNumber+1}</p>
       <p>Number of Neurons</p>
       <input />
       <p>Activation Function</p>
-      <select id="activationFunction" name="activationFunction">
+      <select onChange={selectHandler}>
         <option value="relu">relu</option>
         <option value="sigmoid">sigmoid</option>
         <option value="softmax">softmax</option>

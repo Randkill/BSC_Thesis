ec1da14ab72b86ff5c6bdd72541245b5915ea63a
Randkill <randkill.randkill@gmail.com>
2020-11-14 20:27:49 +0330

    Desgined basics of Model Configuration Component!

--- Visual-Keras-Model-Generator/cheatsheet/FrontEnd/frontend01/src/Components/ModelConfiguration/Configuration.js
+++ Visual-Keras-Model-Generator/cheatsheet/FrontEnd/frontend01/src/Components/ModelConfiguration/Configuration.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
-import './Configuration.css'
+import "./Configuration.css";
 
 const Configuration = () => {
-
-}
+  return (
+    <div className="SuperComponent">
+    <p className="ComponentTitle">Model Configuration</p>
+      <p>Optimizer</p>
+      <input />
+      <p>Loss Function</p>
+      <input />
+      <p>Metrics</p>
+      <input />
+    </div>
+  );
+};
 
 export default Configuration;

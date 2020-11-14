08b79980171665fdc5a52a03a33d3348f72793b3
Randkill <randkill.randkill@gmail.com>
2020-11-14 19:58:27 +0330

    Added some css styles both on css file and js file!

--- Visual-Keras-Model-Generator/cheatsheet/FrontEnd/frontend01/src/Components/Layer/Layer.js
+++ Visual-Keras-Model-Generator/cheatsheet/FrontEnd/frontend01/src/Components/Layer/Layer.js
@@ -3,24 +3,24 @@ import React from "react";
 import "./Layer.css";
 
 const Layer = (props) => {
-
-  const selectHandler = (event) => {
+  const getValue = (event) => {
     return event.target.value;
-  }
+  };
 
   return (
     <div className="Layer">
-      <p className="LayerTitle">Layer {props.layerNumber+1}</p>
+      <p className="LayerTitle">Layer {props.layerNumber + 1}</p>
       <p>Number of Neurons</p>
-      <input onChange={props.neuronChange}/>
+      <input onChange={props.neuronChange} className="input"/>
       <p>Activation Function</p>
-      <select onChange={props.activationFunctionChange}>
+      <select onChange={props.activationFunctionChange} className="select">
         <option value="relu">relu</option>
         <option value="sigmoid">sigmoid</option>
         <option value="softmax">softmax</option>
         <option value="softplus">softplus</option>
       </select>
-      <button onClick={props.delete}>-</button>
+      <br/>
+      <button onClick={props.delete} className="delete">-</button>
     </div>
   );
 };

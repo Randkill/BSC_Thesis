2d015b2bedfc88de7a26a1d5b3d4ff714fa12c17
Randkill <randkill.randkill@gmail.com>
2020-11-14 20:46:39 +0330

    Added visuall effects and completed the design of tags in Model Configuration!

--- Visual-Keras-Model-Generator/cheatsheet/FrontEnd/frontend01/src/Components/ModelConfiguration/Configuration.js
+++ Visual-Keras-Model-Generator/cheatsheet/FrontEnd/frontend01/src/Components/ModelConfiguration/Configuration.js
@@ -7,11 +7,32 @@ const Configuration = () => {
     <div className="SuperComponent">
     <p className="ComponentTitle">Model Configuration</p>
       <p>Optimizer</p>
-      <input />
+      <select className="selectModelConfiguration">
+          <option value="SGD">SGD</option>
+          <option value="RMSprop">RMSprop</option>
+          <option value="Adam">Adam</option>
+          <option value="Adadelta">Adadelta</option>
+          <option value="Adagrad">Adagrad</option>
+          <option value="Adamax">Adamax</option>
+          <option value="Nadam">Nadam</option>
+          <option value="Ftrl">Ftrl</option>
+      </select>
       <p>Loss Function</p>
-      <input />
+      <select className="selectModelConfiguration">
+          <option value="mean_squared_error">Mean Squared Error</option>
+          <option value="mean_absolute_error">Mean Absolute Error</option>
+          <option value="mean_absolute_percentage_error">Mean Absolute Perccentage Error</option>
+          <option value="mean_squared_logarithmic_error">Mean Squared Logarithmic Error</option>
+          <option value="cosine_similarity">Cosine Similarity</option>
+      </select>
       <p>Metrics</p>
-      <input />
+      <select className="selectModelConfiguration">
+          <option value="accuracy">Accuracy</option>
+          <option value="binary_accuracy">Binary Accuracy</option>
+          <option value="categorical_accuracy">Categorical Accuracy</option>
+          <option value="top_k_categorical_accuracy">Top K Categorical Accuracy</option>
+          <option value="sparse_top_k_categorical_accuracy">Sparse Top K Categorical Accuracy</option>
+      </select>
     </div>
   );
 };

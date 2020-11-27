15f148857477a2a473b67b692611aed08c13d019
Randkill <randkill.randkill@gmail.com>
2020-11-27 12:39:41 +0330

    Handling POST, GET and parsing json into final model usable in keras!

--- Visual-Keras-Model-Generator/cheatsheet/BackEnd/app.js
+++ Visual-Keras-Model-Generator/cheatsheet/BackEnd/app.js
@@ -1,6 +1,8 @@
-var bodyParser = require('body-parser');
-
-const express = require('express');
+var bodyParser = require("body-parser");
+const fs = require("fs");
+const express = require("express");
+const spawn = require("child_process").spawn;
+const PythonShell = require("python-shell");
 
 const app = express();
 
@@ -9,25 +11,94 @@ app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(function(req, res, next) { //allow cross origin requests
-    res.setHeader("Access-Control-Allow-Methods", "POST, PUT, OPTIONS, DELETE, GET");
-    res.header("Access-Control-Allow-Origin", "http://localhost:3001");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Credentials", true);
-    next();
+app.use(function (req, res, next) {
+  //allow cross origin requests
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "POST, PUT, OPTIONS, DELETE, GET"
+  );
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
 });
 
-app.post('/post', function(req, res, next){
-    //res.setHeader('Access-Control-Allow-Origin');
 
-    console.log(req.body); // req.body contains the parsed body of the request.
-    res.end('Done')
+
+app.post("/post", function (req, res, next) {
+  //res.setHeader('Access-Control-Allow-Origin');
+
+  console.log(req.body); // req.body contains the parsed body of the request.
+
+  //saving recieved json
+  console.log("Body : \n\n\n", JSON.stringify(req.body));
+  fs.writeFile("./JSON_Handler/data.json", JSON.stringify(req.body), (err) => {
+    if (err) {
+      throw err;
+    }
+    console.log("JSON data is saved.");
+  });
+
+  // const data = modelHandling()
+  // console.log('Data to be sent:', data)
+  res.end('Post Finished!')
+  console.log('POST REQUEST FINISHED !')
+
 });
-app.get('/get', function(req,res, next){
-    console.log('get request completed')
-    res.end('Done')
-})
+app.get("/get", function (req, res, next) {
+  //pythonHandler();
+  console.log("GET request started");
+
+  var data = modelHandling()
+  console.log(data)
+  console.log('GET request Finished!')
+  res.end(data)
+
+});
+
+app.listen(8001, "localhost");
 
 
+const pythonHandler = () => {
+  const { spawn } = require("child_process");
+  const pyProg = spawn("python", ["./JSON_Handler/test.py"]);
 
-app.listen(8000, 'localhost');
+  pyProg.stdout.on("data", function (data) {
+    console.log(data.toString());
+     
+    return data.toString()
+  });
+};
+
+const modelHandling = () => {
+  const data = require("./JSON_Handler/data.json");
+  // console.log(data);
+
+  var model = "model.Sequential()";
+
+  for (i = 0; i < data.layers.length; i++) {
+    model = model.concat(
+      "\nmodel.add(Dense(",
+      data.layers[i].neurons,
+      ', activation="',
+      data.layers[i].activationFunction,
+      '"))'
+    );
+    //console.log("hi");
+  }
+  model = model.concat(
+    '\nmodel.compile(loss="',
+    data.ModelConfig.lossFunction,
+    '", optimizer="',
+    data.ModelConfig.optimizer,
+    '", metrics=["',
+    data.ModelConfig.metrics,
+    '"])'
+  );
+
+  console.log(model)
+  return model;
+};

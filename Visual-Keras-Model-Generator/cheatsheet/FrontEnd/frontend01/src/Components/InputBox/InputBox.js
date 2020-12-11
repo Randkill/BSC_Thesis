0503f6cd036ffdc4342807f4169a2e7bcba57a4c
Randkill <randkill.randkill@gmail.com>
2020-12-11 11:55:29 +0330

    Initialized a component for inputbox!

---
+++ Visual-Keras-Model-Generator/cheatsheet/FrontEnd/frontend01/src/Components/InputBox/InputBox.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './InputBox.css'
+
+const InputBox = () => {
+    return(
+        <div className="input">
+            <p>Enter the input size:</p>
+            <input></input>
+        </div>
+    )
+}
+
+export default InputBox;
